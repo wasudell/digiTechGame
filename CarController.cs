984b3abbc89e62faf2a83b343f6cff33e4fa7dc5
wasudell <98854413+wasudell@users.noreply.github.com>
2022-09-24 22:49:50 +1200

    All Scripts 24/10/22
    Final Assembly, All scripts as of the 24th of September 2022

--- CarController.cs
+++ CarController.cs
@@ -5,58 +5,89 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {   
-    public float horizontalInput;
-    public float verticalInput;
-    public bool isBraking;
-    private float steeringAngle;
-    private float brakeForce;
-    public Vector3 comAdjust;
-    public Quaternion vanRotation;
+    // inputs and outputs for/of van
+    public float horizontalInput; // horizontal (steering) input from player
+    public float verticalInput; // vertical (accelerator) input from player
+    public bool isBraking; // whether or not the brake key is being pressed
+    private float steeringAngle; // steering angle output from steering input values
+    private float brakeForce; // force of brakes
+    public Vector3 comAdjust; // adjustment of the centre of mass of the van
+    public Quaternion vanRotation; // rotation of the van
+    public float motorPower; // power of the motor
+    public float maxSteeringAngle; // maximum angle the wheels can turn
+    public float brakePower; // force of brakes
+    private float carXRot; // X rotation of the van
+    private float carYRot; // Y rotation of the van
+    private float carZRot; // Z rotation of the van
+    public float carVelocity; // velocity of the van
+    public float impulse; // change in momentum in a collision
+    public float health = 100; // health of the van
 
-    public float motorPower = 10000f;
-    public float maxSteeringAngle = 35f;
-    public float brakePower = 1200000f;
-    public float carXRot;
-    public float carZRot;
-
-    public Rigidbody carRigidbody;
-    public Transform car;
-    public Transform wholeCar;
-    public float carVelocity;
-    public WheelCollider FRWheelCollider;
+    // physcial aspects of the van and things attached to it
+    public GameObject bodyGameObject; // game object of the body of the van
+    public GameObject cannonGameObject; // game object of the cannon
+    public GameObject wheelGameObjects; // all game objects of the wheels
+    // all above game objects are for getting destroyed in the explosion
+    private Rigidbody carRigidbody; // rigidbody of the van
+    public Transform car; // transform of the body of the van
+    public Transform wholeCar; // transform of the whole van
+    public WheelCollider FRWheelCollider; // wheel colliders of all the wheels
     public WheelCollider FLWheelCollider;
     public WheelCollider BRWheelCollider;
     public WheelCollider BLWheelCollider;
-    public Transform FRWheelTransform;
+    public Transform FRWheelTransform; // transforms (visuals) of all the wheels
     public Transform FLWheelTransform;
     public Transform BRWheelTransform;
     public Transform BLWheelTransform;
+
+    // Audio
+    private AudioSource sound; // thing on the van that produces the sound (speaker) which is picked up by
+    // an audio listener (microphone) on the camera
+    private AudioSource collisionSound; // getting audio source on body to produce the collision sound
+    public UIScore score; // score script in order to find when the pause menu is activated so sound can be paused
+
+    // explosion and game over things
+    public ParticleSystem explosion; // particle system that plays on death (explosion)
+    private bool deathCheck; // a checker to show when the player is dead (health = 0)
+    public bool endScreen = false; // whether or not the end screen should appear
 
     private void Start()
     {
         // declaring centre of mass so it can be adjsuted
         carRigidbody = GetComponent<Rigidbody>();
+        // getting the audio source attached to the van
+        sound = GetComponent<AudioSource>();
+        // getting the audio souce on the body object, which produces the collision sound
+        // this simplifies things as compared to adding two sources on the van
+        collisionSound = bodyGameObject.GetComponent<AudioSource>();
+        // resetting the deathCheck cause van is alive at start
+        deathCheck = false;
     }
     private void FixedUpdate()
-    {
-        GetInput();
-        handleMotor();
-        Steering();
-        UpdateWheels();
+    { 
+        GetInput(); // getting the input from the player or sensors
+        handleMotor(); // translating those inputs into moving the van
+        Steering(); // translating steering inputs into steering
+        UpdateWheels(); // updating the visuals of the wheels
     }
     private void GetInput()
     {
-        // declaring rotation for the cannon
-        vanRotation = transform.rotation;
         // adjusting centre of mass
         carRigidbody.centerOfMass = comAdjust;
         carRigidbody.WakeUp();
+
         // getting input of forward, backward, left and right (W, A, S, D)
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
-        // finding car velocity
+
+        // finding van's velocity
         carVelocity = transform.InverseTransformDirection(carRigidbody.velocity).z;
-        // finding if the car is going forward, backward, or is still and using W or S to brake accordingly
+        // limiting the van's speed
+        if (carVelocity > 35){
+            verticalInput = 0;
+        }
+
+        // finding if the van is going forward, backward, or is still and using W or S to brake accordingly
         if (carVelocity > 0.05){
             isBraking = Input.GetKey(KeyCode.S);
         }
@@ -67,68 +98,121 @@ public class CarController : MonoBehaviour
             isBraking = Input.GetKey(KeyCode.W);
             isBraking = Input.GetKey(KeyCode.S);
         }
-        // converting quaternion rotation to euler angles
-        float X = car.transform.rotation.x;
-        float Y = car.transform.rotation.y;
-        float Z = car.transform.rotation.z;
-        float W = car.transform.rotation.w;
-        float carXRotRad = Mathf.Atan2(2*X*W-2*Y*Z , 1 - 2*X*X - 2*Z*Z);
-        carXRot = Mathf.Rad2Deg * carXRotRad + 90;
-        float carZRotRad = Mathf.Asin(2*X*Y + 2*Z*W);
-        carZRot = Mathf.Rad2Deg * carZRotRad;
-        // finding if the car is flipped and if the flipped key is pressed
+        
+        // declaring rotation for the cannon
+        vanRotation = transform.rotation;
+        // declaring the individual rotation values of the van
+        carXRot = transform.rotation.eulerAngles.x;
+        carYRot = transform.rotation.eulerAngles.y;
+        carZRot = transform.rotation.eulerAngles.z;
+        // finding if the van is flipped and if the flipped key is pressed
         bool flipCar = Input.GetKey(KeyCode.F);
         if (((carXRot > 175 & carXRot < 185) || carZRot > 65 || carZRot < -65) & flipCar == true){
             if (carZRot > 65){  // on its left side
             wholeCar.transform.position = Vector3.Lerp(wholeCar.transform.position, wholeCar.TransformPoint(1, 0, 0), 1);
-            wholeCar.transform.rotation = Quaternion.Lerp(wholeCar.transform.rotation, new Quaternion(0, 0, 0, 1), 1);
+            wholeCar.transform.rotation = Quaternion.Lerp(wholeCar.transform.rotation, Quaternion.Euler(0, carYRot, 0), 1);
             return;
             }
             if (carZRot < -65){  // on its right side
             wholeCar.transform.position = Vector3.Lerp(wholeCar.transform.position, wholeCar.TransformPoint(1, 0, 0), 1);
-            wholeCar.transform.rotation = Quaternion.Lerp(wholeCar.transform.rotation, new Quaternion(0, 0, 0, 1), 1);
+            wholeCar.transform.rotation = Quaternion.Lerp(wholeCar.transform.rotation, Quaternion.Euler(0, carYRot, 0), 1);
             return;
             }
             if (carXRot > 175 & carXRot < 185){ // upside down
             wholeCar.transform.position = Vector3.Lerp(wholeCar.transform.position, wholeCar.TransformPoint(0, -1, 0), 1);
-            wholeCar.transform.rotation = Quaternion.Lerp(wholeCar.transform.rotation, new Quaternion(0, 0, 0, 1), 1);
+            wholeCar.transform.rotation = Quaternion.Lerp(wholeCar.transform.rotation, Quaternion.Euler(0, carYRot, 0), 1);
             return;
             }
             return;
         }
+
+        // exploding the van if health is equal to or less than 0
+        if (health <= 0 && deathCheck == false){
+            Explode();
+            // making sure the explode script only runs once
+            deathCheck = true;
+            // turning off the engine sound upon death
+            sound.mute = !sound.mute;
+        }
+
+        // increasing the sound's (engine's) pitch as the van moves faster, as that makes it sounds like the engine is working harder and making it move faster
+        sound.pitch = 1 + ((carVelocity / 100) * verticalInput); // multiplied by vertical input so that if the accelerator key isn't being pressed, it doesn't sound like the engine is working hard
+        if (carVelocity >= 30){
+            sound.pitch = 1 + 30/100; // vertical input is set to 0 above 30 to set a max speed so this ensures that the engine doesn't sound like it's turning off
+        }
     }
+
     private void handleMotor()
     {
-        FRWheelCollider.motorTorque = verticalInput * motorPower;
+        FRWheelCollider.motorTorque = verticalInput * motorPower; // moving the van forward based on accelerator input and the power of the van's motor
         FLWheelCollider.motorTorque = verticalInput * motorPower;
         BRWheelCollider.motorTorque = verticalInput * motorPower;
         BLWheelCollider.motorTorque = verticalInput * motorPower;
 
-        brakeForce = isBraking ? brakePower : 0f;
-        FRWheelCollider.brakeTorque = brakeForce;
+
+        brakeForce = isBraking ? brakePower : 0f; // turning the brakes on if the brake input is pressed
+        FRWheelCollider.brakeTorque = brakeForce; // applying that value as a force
         FLWheelCollider.brakeTorque = brakeForce;
         BRWheelCollider.brakeTorque = brakeForce;
         BLWheelCollider.brakeTorque = brakeForce;
     }
+
     private void Steering()
     {
-        steeringAngle = maxSteeringAngle * horizontalInput;
-        FRWheelCollider.steerAngle = steeringAngle;
+        steeringAngle = maxSteeringAngle * horizontalInput; // steering the van based on the steering input and the max steering the van can do
+        FRWheelCollider.steerAngle = steeringAngle; // telling the wheel colliders to steer
         FLWheelCollider.steerAngle = steeringAngle;
     }
+
     private void UpdateWheels()
     {
-        UpdateSingleWheel(FLWheelCollider, FLWheelTransform);
+        UpdateSingleWheel(FLWheelCollider, FLWheelTransform); // updating the visuals of the wheels based on steering angle
         UpdateSingleWheel(FRWheelCollider, FRWheelTransform);
         UpdateSingleWheel(BLWheelCollider, BLWheelTransform);
         UpdateSingleWheel(BRWheelCollider, BRWheelTransform);
     }
+    
     private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
     {
+        // matching the wheel visual's position and rotation to that of the wheel collider
         Vector3 pos;
         Quaternion rot;
         wheelCollider.GetWorldPose(out pos, out rot);
         wheelTransform.rotation = rot;
         wheelTransform.position = pos;
+    }
+
+    void OnCollisionEnter(Collision crash){ // called when the van collides with another object
+        // getting impulse of collision (change in momentum)
+        impulse = Mathf.Abs(crash.impulse.x) + Mathf.Abs(crash.impulse.y) + Mathf.Abs(crash.impulse.z);
+        // minusing impulse from health (divided by 10000 as impulse is a big number)
+        health = health - impulse / 10000;
+        // playing the collision sound
+        collisionSound.Play();
+        // changing the volume of the collision sound based on how hard the collision was (impulse)
+        if ((impulse / 100000) < 1){ // making sure the volume doesn't go above 1
+            collisionSound.volume = impulse / 100000;
+        }
+        else {
+            collisionSound.volume = 1;
+        }
+    }
+
+    private void Explode(){ // called when health = 0
+        // creating the explosion
+        Instantiate(explosion, transform);
+        // telling the explosion particle system to play
+        explosion.Play();
+        // making the van disappear
+        Destroy(bodyGameObject, 0f);
+        Destroy(cannonGameObject, 0f);
+        Destroy(wheelGameObjects, 0f);
+        // Making the end screen wait a few seconds to activate
+        StartCoroutine(EndScreenWait());
+    }
+
+    IEnumerator EndScreenWait(){ // telling the end screen to show after a few seconds so it doesn't appear immediately
+        yield return new WaitForSeconds(3);
+        endScreen = true; // indicates to Score.cs script to show the end screen
     }
 }
--- UIScore.cs
+++ UIScore.cs
@@ -26,13 +26,17 @@ public class UIScore : MonoBehaviour
     public Image background;
     private Color colour;
     public Button playAgainButton;
+    public Button mainMenuButton;
+    public Button resumeButton;
     public Text highScoreText;
     public Text gameOverText;
-    public Button mainMenuButton;
+    // pause menu checker
+    public bool pauseMenu = false;
 
     void Start(){
         // getting the current high score
         highScore = PlayerPrefs.GetInt("highScore");
+
         // end screen background
         // setting the size of the background to the screen size
         background.rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
@@ -40,28 +44,42 @@ public class UIScore : MonoBehaviour
         colour = background.color;
         colour.a = 0f;
         background.color = colour;
+
         // play again button
         // setting the button way off the screen so it isn't visible
         playAgainButton.transform.position = new Vector2(9999, 0);
         // allows the button to be clicked
         playAgainButton.onClick.AddListener(PlayAgainClick);
+
+        // resume button
+        // setting the button way off the screen so it isn't visible
+        resumeButton.transform.position = new Vector2(9999, 0);
+        // allows the button to be clicked
+        resumeButton.onClick.AddListener(ResumeClick);
+
         // main menu button
         // same as play again button
         mainMenuButton.transform.position = new Vector2(9999, 0);
         // allows button to be clicked as well
         mainMenuButton.onClick.AddListener(MainMenuClick);
+
         // high score text
         // making the text invisible at start
         highScoreText.text = "";
+
         // game over text
         // making the text invisible
         gameOverText.text = "";
+        // moving the text in place
+        gameOverText.transform.position = new Vector2(gameOverText.transform.position.x, gameOverText.transform.position.y - (Screen.height / 6)); // 1/6th of the screen down
+
     }
     
     void Update()
     {
         // updating the text box every frame to the correct score
         txt.text = "Score: " + score;
+
         // destroying the UI
         if (van.endScreen == true){
             Destroy(Address);
@@ -73,37 +91,78 @@ public class UIScore : MonoBehaviour
             Destroy(border);
             Destroy(minimap);
             Destroy(marker);
+
             // moving the text down
             transform.position = new Vector2(transform.position.x - (Screen.width / 6), transform.position.y - (0.35f * Screen.height)); // 35% of screen down, and 1/6th to the left
+
             // background
             // making background visible again
             colour.a = 1f;
             background.color = colour;
+
             // play again button
             // moving the play again button
             playAgainButton.transform.position = new Vector2(Screen.width / 2, Screen.height / 2); // setting position to centre of screen
             playAgainButton.transform.position = new Vector2(playAgainButton.transform.position.x, playAgainButton.transform.position.y - (Screen.height / 12)); // down 1/12th of screen height
+
             // main menu button
             mainMenuButton.transform.position = new Vector2(Screen.width / 2, Screen.height / 2); // setting position to centre of screen
             mainMenuButton.transform.position = new Vector2(mainMenuButton.transform.position.x, mainMenuButton.transform.position.y - (Screen.height / 4)); // down 1/4th of screen height
+
             // high score text
             // bringing up the text once the end screen is called (player dies)
             highScoreText.text = "High Score: " + highScore;
             // moving the text
             highScoreText.transform.position = new Vector2(highScoreText.transform.position.x + (Screen.width / 6), highScoreText.transform.position.y - (0.35f * Screen.height)); // 1/6th to the right and 35% down (of screen)
+
             // game over text
             // making the text visible again
             gameOverText.text = "Game Over!";
-            // moving the text in place
-            gameOverText.transform.position = new Vector2(gameOverText.transform.position.x, gameOverText.transform.position.y - (Screen.height / 6)); // 1/6th of the screen down
 
             // making the code not loop
             van.endScreen = false;
         }
+
         // changing the highscore if the current score is higher
         if (score > highScore){
             highScore = score;
             PlayerPrefs.SetInt("highScore", highScore);
+        }
+
+        // Pause Menu
+        if (Input.GetKeyDown("escape") && pauseMenu == false){ // get button down so the code doesn't get run over multiple frames
+            // setting so that things don't change when escape is pressed again, timed as will open and close menu in same frame if not
+            StartCoroutine(wait());
+
+            // setting the time to 0 so that the game pauses in the background
+            Time.timeScale = 0.000001f;
+
+            // game over text (using game over text as it is basically the same thing)
+            // making the text visible again
+            gameOverText.text = "Game Paused";
+
+            // background
+            // making background visible again
+            colour.a = 1f;
+            background.color = colour;
+
+            // main menu button
+            mainMenuButton.transform.position = new Vector2(Screen.width / 2, Screen.height / 2); // setting position to centre of screen
+            mainMenuButton.transform.position = new Vector2(mainMenuButton.transform.position.x, mainMenuButton.transform.position.y - (Screen.height / 4)); // down 1/4th of screen height
+
+            // resume button
+            // moving the resume button
+            resumeButton.transform.position = new Vector2(Screen.width / 2, Screen.height / 2); // setting position to centre of screen
+            resumeButton.transform.position = new Vector2(resumeButton.transform.position.x, resumeButton.transform.position.y - (Screen.height / 12)); // down 1/12th of screen height
+
+            // turning off the game's audio
+            AudioListener.pause = true;
+        }
+
+        // setting so that if escape is pressed when the pause menu is active, the game resumes
+        if (Input.GetKeyDown("escape") && pauseMenu == true){
+            // same function as resume button
+            ResumeClick(); // doesn't need waiter as it is placed after pause menu opener so wont run until next frame
         }
     }
 
@@ -124,10 +183,48 @@ public class UIScore : MonoBehaviour
     }
 
     void PlayAgainClick(){
+        // reloading the game if play again is pressed
         SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
     }
 
     void MainMenuClick(){
+        // loading the main menu if main menu is pressed
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    void ResumeClick(){
+        // resuming the game if the resume button is clicked
+
+        // setting the time back to normal
+        Time.timeScale = 1;
+
+        // setting everything back to as it was as the game is playing
+        // making the background invisible when the game is playing
+        colour = background.color;
+        colour.a = 0f;
+        background.color = colour;
+
+        // resume button
+        // setting the button way off the screen so it isn't visible
+        resumeButton.transform.position = new Vector2(9999, 0);
+
+        // main menu button
+        // same as play again button
+        mainMenuButton.transform.position = new Vector2(9999, 0);
+
+        // game over text
+        // making the text invisible
+        gameOverText.text = "";
+
+        // allowing pause menu to be reactivated
+        pauseMenu = false;
+
+        // resuming the game's audio
+        AudioListener.pause = false;
+    }
+
+    IEnumerator wait(){ // see line 134
+        yield return new WaitForSeconds(0.0000001f);
+        pauseMenu = true;
     }
 }
